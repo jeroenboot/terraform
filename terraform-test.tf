f6d7ba91ee963e06d08fda3945b6f5740a6191bd
Jeroen Boot <jeroenboot@YWCMac076.local>
2019-09-09 16:52:52 +0200

    multiple servers

--- terraform-test.tf
+++ terraform-test.tf
@@ -31,8 +31,39 @@ data "vsphere_network" "network" {
   datacenter_id = "${data.vsphere_datacenter.dc.id}"
 }
 
+
+
+
+
+
 resource "vsphere_virtual_machine" "vm" {
-  name             = "terraform-test2"
+  name             = "terraform-vm1"
+  resource_pool_id = "${data.vsphere_resource_pool.pool.id}"
+  datastore_id     = "${data.vsphere_datastore.datastore.id}"
+  wait_for_guest_net_timeout = "0" #disable check for valid IP address
+
+  num_cpus = 2
+  memory   = 512
+  guest_id = "other3xLinux64Guest"
+
+  network_interface {
+    network_id = "${data.vsphere_network.network.id}"
+  }
+
+  cdrom {
+    datastore_id     = "${data.vsphere_datastore.datastore.id}"
+    path             = "ISO/TinyCore-current.iso"
+  }
+
+  disk {
+    label = "disk0"
+    size  = 20
+  }
+}
+
+
+resource "vsphere_virtual_machine" "vm" {
+  name             = "terraform-vm2"
   resource_pool_id = "${data.vsphere_resource_pool.pool.id}"
   datastore_id     = "${data.vsphere_datastore.datastore.id}"
   wait_for_guest_net_timeout = "0" #disable check for valid IP address
