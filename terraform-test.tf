03ba437a480f52ca538899211c8d050dc42a3c40
Jeroen Boot <jeroenboot@YWCMac076.local>
2019-09-09 15:48:36 +0200

    Added some comments

--- terraform-test.tf
+++ terraform-test.tf
@@ -8,26 +8,26 @@ provider "vsphere" {
 }
 
 data "vsphere_datacenter" "dc" {
-  name = "Datacenter"
+  name = "Datacenter" #name of the datacenter
 }
 
 data "vsphere_datastore" "datastore" {
-  name          = "datastore1"
+  name          = "datastore1" #name of the datastore
   datacenter_id = "${data.vsphere_datacenter.dc.id}"
 }
 
 data "vsphere_host" "host" {
-  name          = "192.168.101.10"
+  name          = "192.168.101.10" #host to deploy VM on
   datacenter_id = "${data.vsphere_datacenter.dc.id}"
 }
 
 data "vsphere_resource_pool" "pool" {
-  name          = "Cluster1"
+  name          = "Cluster1" #name of the resource_pool
   datacenter_id = "${data.vsphere_datacenter.dc.id}"
 }
 
 data "vsphere_network" "network" {
-  name          = "VM Network"
+  name          = "VM Network" #name of the virtual_network
   datacenter_id = "${data.vsphere_datacenter.dc.id}"
 }
 
@@ -35,7 +35,7 @@ resource "vsphere_virtual_machine" "vm" {
   name             = "terraform-test2"
   resource_pool_id = "${data.vsphere_resource_pool.pool.id}"
   datastore_id     = "${data.vsphere_datastore.datastore.id}"
-  wait_for_guest_net_timeout = "0"
+  wait_for_guest_net_timeout = "0" #disable check for valid IP address
 
   num_cpus = 2
   memory   = 512
