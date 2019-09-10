2a935a2c5901d6bc41ed0124b8c1c31b3a4b2f5c
Jeroen Boot <jeroenboot@YWCMac076.local>
2019-09-10 14:10:23 +0200

    Adjusted vsca

---
+++ vm1.tf
@@ -0,0 +1,24 @@
+resource "vsphere_virtual_machine" "vm1" {
+  name             = "terraform-vm1"
+  resource_pool_id = "${data.vsphere_resource_pool.pool.id}"
+  datastore_id     = "${data.vsphere_datastore.datastore.id}"
+  wait_for_guest_net_timeout = "0" #disable check for valid IP address
+
+  num_cpus = 2
+  memory   = 512
+  guest_id = "other3xLinux64Guest"
+
+  network_interface {
+    network_id = "${data.vsphere_network.network.id}"
+  }
+
+  cdrom {
+    datastore_id     = "${data.vsphere_datastore.datastore.id}"
+    path             = "ISO/TinyCore-current.iso"
+  }
+
+  disk {
+    label = "disk0"
+    size  = 20
+  }
+}
--- vsphere_infra.tf
+++ vsphere_infra.tf
@@ -17,7 +17,7 @@ data "vsphere_datastore" "datastore" {
 }
 
 data "vsphere_host" "host" {
-  name          = "192.168.101.10" #host to deploy VM on
+  name          = "host1" #host to deploy VM on
   datacenter_id = "${data.vsphere_datacenter.dc.id}"
 }
 
@@ -29,60 +29,4 @@ data "vsphere_resource_pool" "pool" {
 data "vsphere_network" "network" {
   name          = "VM Network" #name of the virtual_network
   datacenter_id = "${data.vsphere_datacenter.dc.id}"
-}
-
-
-
-
-
-
-resource "vsphere_virtual_machine" "vm1" {
-  name             = "terraform-vm1"
-  resource_pool_id = "${data.vsphere_resource_pool.pool.id}"
-  datastore_id     = "${data.vsphere_datastore.datastore.id}"
-  wait_for_guest_net_timeout = "0" #disable check for valid IP address
-
-  num_cpus = 2
-  memory   = 512
-  guest_id = "other3xLinux64Guest"
-
-  network_interface {
-    network_id = "${data.vsphere_network.network.id}"
-  }
-
-  cdrom {
-    datastore_id     = "${data.vsphere_datastore.datastore.id}"
-    path             = "ISO/TinyCore-current.iso"
-  }
-
-  disk {
-    label = "disk0"
-    size  = 20
-  }
-}
-
-
-resource "vsphere_virtual_machine" "vm2" {
-  name             = "terraform-vm2"
-  resource_pool_id = "${data.vsphere_resource_pool.pool.id}"
-  datastore_id     = "${data.vsphere_datastore.datastore.id}"
-  wait_for_guest_net_timeout = "0" #disable check for valid IP address
-
-  num_cpus = 2
-  memory   = 512
-  guest_id = "other3xLinux64Guest"
-
-  network_interface {
-    network_id = "${data.vsphere_network.network.id}"
-  }
-
-  cdrom {
-    datastore_id     = "${data.vsphere_datastore.datastore.id}"
-    path             = "ISO/TinyCore-current.iso"
-  }
-
-  disk {
-    label = "disk0"
-    size  = 20
-  }
 }
