4696b4bc8d82eb98e44e7e5daf71af8b558411f1
Jeroen Boot <jeroenboot@YWCMac076.local>
2019-09-11 14:38:14 +0200

    adjusted vm2

--- vm2.tf
+++ vm2.tf
@@ -1,12 +1,12 @@
 resource "vsphere_virtual_machine" "vm2" {
-  name             = "terraform-vm2"
+  name             = "ubuntu1804live-vm2"
   resource_pool_id = "${data.vsphere_resource_pool.pool.id}"
   datastore_id     = "${data.vsphere_datastore.datastore.id}"
   wait_for_guest_net_timeout = "0" #disable check for valid IP address
 
   num_cpus = 2
-  memory   = 512
-  guest_id = "other3xLinux64Guest"
+  memory   = 2048
+  guest_id = "ubuntu64Guest"
 
   network_interface {
     network_id = "${data.vsphere_network.network.id}"
@@ -17,8 +17,4 @@ resource "vsphere_virtual_machine" "vm2" {
     path             = "ISOs/TinyCore-current.iso"
   }
 
-  disk {
-    label = "disk0"
-    size  = 20
-  }
 }
